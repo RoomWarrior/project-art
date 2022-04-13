cfc4d2ad0e692dac04244d90b39f4c30da335923
Arthur <hiatsuaoikarma@gmail.com>
2022-04-13 18:07:46 +0600

    Initial commit

---
+++ src/test/java/io/hiatsu/projectart/ProjectArtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hiatsu.projectart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
